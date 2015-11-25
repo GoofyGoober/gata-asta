0c4fdb04ce637eb64a6670a34f4f21bfc4d51173
Alessandro <descovi@gmail.com>
2015-11-25 13:11:12 +0100

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124191859) do
+ActiveRecord::Schema.define(version: 20151125114553) do
+
+  create_table "details", force: :cascade do |t|
+    t.integer  "opera_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "operas", force: :cascade do |t|
     t.text     "name"
