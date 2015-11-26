84b20ec6084ba19e9f964b7475d4109baf031ce3
Alessandro <descovi@gmail.com>
2015-11-26 14:10:45 +0100

    Opzioni per comprimere la foto.

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,9 +1,16 @@
 class Detail < ActiveRecord::Base
-  has_attached_file :image, styles: { 
+  has_attached_file :image, 
+  styles: { 
     normal: "1024x1024>", 
     medium: "300x300>", 
     thumb: "100x100>",
     big: "600x600>"
+  },  
+  convert_options: {
+    normal: '-quality 80 -strip',
+    medium: '-quality 80 -strip',
+    thumb: '-quality 60 -strip',
+    big: '-quality 80 -strip'
   }
   belongs_to :opera
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
