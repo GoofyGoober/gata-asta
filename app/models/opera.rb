7552cb70f1bc69c7f971f1f702dfc45e0fb73ef6
Alessandro <descovi@gmail.com>
2015-11-30 15:41:15 +0100

    Quality also for other models

--- app/models/opera.rb
+++ app/models/opera.rb
@@ -5,6 +5,13 @@ class Opera < ActiveRecord::Base
     thumb: "100x100>",
     big: "600x600>",
     quad2: "300x300#"
+  },  
+  convert_options: {
+    normal: '-quality 90 -strip',
+    medium: '-quality 80 -strip',
+    thumb: '-quality 60 -strip',
+    big: '-quality 80 -strip',
+    quad2: '-quality 80 -strip'
   }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :archive_index, :image, :pannello, presence: true
--- app/models/pannello.rb
+++ app/models/pannello.rb
@@ -4,6 +4,12 @@ class Pannello < ActiveRecord::Base
     big: "600x600>",
     medium: "300x300>", 
     thumb: "100x100>" 
+  },  
+  convert_options: {
+    normal: '-quality 90 -strip',
+    medium: '-quality 80 -strip',
+    thumb: '-quality 60 -strip',
+    big: '-quality 80 -strip'
   }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_attachment_presence :image
