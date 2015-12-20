8e9f94070006945f247640835a132b5a57b0dc4e
Alessandro <descovi@gmail.com>
2015-12-20 12:39:30 +0100

    mostra indice di archivio

--- app/controllers/operas_controller.rb
+++ app/controllers/operas_controller.rb
@@ -1,6 +1,6 @@
 class OperasController < ApplicationController
   def index
-    @operas = Opera.all
+    @operas = Opera.all.order(:archive_index)
   end
   def show
     @opera = Opera.find params[:id]
